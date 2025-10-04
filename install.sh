69da417ee76212bdf173ba28ecc41d0c43283d75
Iñaki Murillo Arroyo <inakimurillo@gmail.com>
2025-10-04 16:15:10 +0200

    Add simple script to install LazyVim

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo " LazyVim installer and configuration"
+echo ""
+
+echo "Download Neovim"
+wget https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
+
+read -sp "Enter sudo password to copy Neovim in /opt/: " PASSWORD
+echo "$PASSWORD" | sudo -S tar -xvf nvim-linux-x86_64.tar.gz -C /opt/
+
+echo "Clean downloaded file"
+rm nvim-linux-x86_64.tar.gz
+
+echo "Add path to .bashrc"
+echo 'export PATH="$PATH;/opt/nvim-linux-x86_64/bin/"' >>.bashrc
+
+echo "Install LazyVim"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
